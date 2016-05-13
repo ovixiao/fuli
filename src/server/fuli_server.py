a4f2d6ce16ee1be13c92cc8ebf4a8d0d2f7789e7
moonlet <xiaorixin@gmail.com>
2016-05-13 23:12:15 +0800

    Pretty my code.

--- src/server/fuli_server.py
+++ src/server/fuli_server.py
@@ -2,16 +2,14 @@
 # -*- encoding: utf-8 -*-
 import os
 import sys
-import json
-import math
-import pymongo
 import argparse
-from bson.objectid import ObjectId
 from flask import Flask, request
 from flask import render_template
 
 
 def add_python_path(path):
+    """Add `libs` path to PYTHONPATH
+    """
     lib_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
     if lib_path != sys.path[0]:
         sys.path.insert(0, lib_path)
@@ -19,63 +17,83 @@ def add_python_path(path):
 
 add_python_path('../libs')
 import db
-import config
 import log
 import utils
 
+TITLE = u'福利聚合'
+DESC = u'各种福利滑滑就有'
+LN = 5
+logger = log.get_logger('fuli')
 
 app = Flask(__name__)
 
 
 def get_posts(page=1, ln=10):
+    """Get posts of tumblr.
+
+    Args:
+        page: the page number. default is 1.
+        ln: item number per page. default is 10.
+
+    Returns:
+        return a generator including the processed post informations.
+    """
+    page = int(page)
+    ln = int(ln)
+    # tumblr collection
     tumblr = db.get_collection('tumblr')
     skip = (page - 1) * ln
     items = tumblr.find(
         {'type': 'video', 'video_url': {'$exists': True}},
-        projection={'player': False},
+        projection={'video_url': True, 'thumbnail_url': True,
+                    'timestamp': True, 'blog_name': True, 'short_url': True,
+                    'duration': True},
         skip=skip, limit=ln, sort=[('date', -1)]
     )
-    ret_items = []
     for item in items:
         dur = item['duration']
-        item['duration'] = '{:02d}:{:02d}'.format(dur / 60, dur % 60)
+        item['duration'] = '{0:02d}:{1:02d}'.format(*divmod(dur, 60))
         item['date'] = utils.pretty_date(item['timestamp'])
-        ret_items.append(item)
-
-    return ret_items
+        yield item
 
 
 @app.route('/')
 @app.route('/page/<p>')
 def page(p=1):
-    p = int(p)
-    ln = 10
-    page_navi = PageNavi(page, 785 / ln)
-    navi_items = gen_navi_items()
-    posts = get_posts(p, ln)
-    return render_template(
-        'page.html',
-        title=u"福利聚合",
-        description=u"宅男爱美女",
-        cur_page=p,
-    )
+    """Page contents.
+
+    Args:
+        p: the page number. default is 1.
+    """
+    posts = get_posts(p, LN)
+    logger.info(uri='page', p=p)
+    return render_template('page.html', title=TITLE, description=DESC,
+                           cur_page=int(p))
 
 
 @app.route('/append/<p>')
 def append(p):
-    p = int(p)
-    ln = 10
-    posts = get_posts(p, ln)
-    return render_template(
-        'append.html',
-        posts=posts,
-    )
+    """Generate data for appending.
+
+    Args:
+        p: the page number.
+    """
+    posts = get_posts(p, LN)
+    logger.info(uri='append', p=p)
+    return render_template('append.html', posts=posts)
 
 
-if __name__ == '__main__':
+def main():
+    """Command line.
+    """
     parser = argparse.ArgumentParser(description='download logs')
+    # debug model
     parser.add_argument('--debug', action='store_true', dest='debug',
-                        help='Start server in debug mode', default=False)
+                        help='Start server in DEBUG mode', default=False)
     args = parser.parse_args()
 
     app.run(debug=args.debug, port=8888)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/server/static/func.js
@@ -0,0 +1,31 @@
+function addPage(page) {
+  $.get("/append/" + page, function(data) {
+    $('.infinite-scroll-bottom .cards').append(data);
+  });
+}
+
+function infinite_scroll() {
+  // 如果正在加载，则退出
+  if (loading) return;
+  // 设置flag
+  loading = true;
+  console.log(curPage);
+  setTimeout(function() {
+    // 重置加载flag
+    if (curPage > maxPage) {
+      // 加载完毕，则注销无限加载事件，以防不必要的加载
+      $.detachInfiniteScroll($('.infinite-scroll'));
+      // 删除加载提示符
+      $('.infinite-scroll-preloader').remove();
+      $('#next-page').attr('href', '/page/' + curPage);
+      $('#next-page').css('display', 'block');
+      return;
+    }
+    // 添加新条目
+    addPage(curPage);
+    curPage += 1;
+    //容器发生改变,如果是js滚动，需要刷新滚动
+    $.refreshScroller();
+    loading = false;
+  }, 500);
+}
